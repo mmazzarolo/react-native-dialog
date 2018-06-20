40c404b1f08ab3ff3bcd83c08f7aadcb47094945
Davide Briani <johnnytheworm@gmail.com>
2018-06-20 10:45:36 +0200

    Add Switch component

---
+++ src/Switch.js
@@ -0,0 +1,56 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Platform, StyleSheet, Switch, Text, View } from "react-native";
+
+export default class DialogSwitch extends React.PureComponent {
+  static propTypes = {
+    ...Switch.propTypes,
+    label: PropTypes.string
+  };
+
+  static displayName = "DialogSwitch";
+
+  render() {
+    const { label, ...otherProps } = this.props;
+    return (
+      <View style={styles.switchWrapper}>
+        <Text style={styles.label}>{label}</Text>
+        <Switch {...otherProps} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  switchWrapper: Platform.select({
+    ios: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginHorizontal: 20,
+      marginBottom: 14,
+      paddingHorizontal: 8
+    },
+    android: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginHorizontal: 10,
+      marginBottom: 20
+    }
+  }),
+  label: Platform.select({
+    ios: {
+      flex: 1,
+      paddingRight: 8,
+      fontSize: 13,
+      color: "black"
+    },
+    android: {
+      flex: 1,
+      paddingRight: 8,
+      fontSize: 16,
+      color: "#33383D"
+    }
+  })
+});
