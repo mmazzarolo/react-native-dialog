6065300746780cd3f8cee071d189fbcbb240d899
Emin <eminsr1905@gmail.com>
2021-10-20 13:36:03 +0300

    codeinput exported

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import DialogButton from "./Button";
 import DialogContainer from "./Container";
 import DialogDescription from "./Description";
 import DialogInput from "./Input";
+import DialogCodeInput from "./CodeInput";
 import DialogSwitch from "./Switch";
 import DialogTitle from "./Title";
 
@@ -10,6 +11,7 @@ export default {
   Container: DialogContainer,
   Description: DialogDescription,
   Input: DialogInput,
+  CodeInput: DialogCodeInput,
   Switch: DialogSwitch,
   Title: DialogTitle,
 };
